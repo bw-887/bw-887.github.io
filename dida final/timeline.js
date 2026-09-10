fe2896aaaf975a22840e90a6a6ffd60b349c278c
bw-887 <wongbranden887@gmail.com>
2026-09-10 01:01:05 -0400

    Add files via upload

---
+++ dida final/timeline.js
@@ -0,0 +1,71 @@
+// gsap scroll trigger
+gsap.registerPlugin(ScrollTrigger);
+window.addEventListener("load", () => {
+    gsap.registerPlugin(ScrollToPlugin);
+
+    // Scroll to bottom of the page smoothly
+    gsap.to(window, {
+        scrollTo: {y: document.body.scrollHeight},
+        duration: 1
+    });
+});
+
+// Fetch event data from JSON
+fetch('events.json')
+    .then(response => response.json() )
+    .then(events => {
+        const timeline = document.getElementById('timeline');
+        events.forEach((event, index) => {
+            const eventDiv = document.createElement('div');
+            eventDiv.className = `event ${ index % 2 === 0 ? 'event-top' : 'event-bottomn' }`;
+
+            eventDiv.innerHTML = `
+            <a href="${event.link}" style="color:white;">
+            <img src="${event.image}" alt="${event.title}">
+            </a>
+            <div class="event-content">
+              <h3 class="event-title">${event.title}</h3>
+              <p class="event-date">${event.date}</p>
+              <p class="event-description">${event.description}</p>
+            </div>
+          `;
+
+            timeline.appendChild(eventDiv);
+        });
+        initializeScrollTrigger();
+    })
+    
+    .catch(error => console.error('Error loading events:', error));
+
+ScrollTrigger.refresh();
+
+
+
+    
+function initializeScrollTrigger() {
+    const timeline = document.querySelector('.timeline');
+    const timelineContainer = document.querySelector('.timeline-container');
+
+    gsap.to(timeline, {
+        x: () => (timeline.scrollWidth - window.innerWidth) + "px", 
+        ease: "none",
+        scrollTrigger: {
+            trigger: timelineContainer,
+            start: "top top",
+            end: () => "+=" + (timeline.scrollWidth - window.innerWidth),
+            scrub: true,
+        },
+    });
+}
+
+// window.addEventListener("load", () => {
+//     gsap.registerPlugin(ScrollToPlugin);
+
+//     // Scroll to bottom of the page smoothly
+//     gsap.to(window, {
+//         scrollTo: {y: document.body.scrollHeight},
+//         duration: 1
+//     });
+// });
+
+       
